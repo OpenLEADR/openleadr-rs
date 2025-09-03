054e1db91297cb2e1febfd7d8a2a4b15b03c86a4
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-09-03 16:26:59 +0200

    Update openleadr-wire for OpenADR 3.1
    Signed-off-by: Björn Baron <bjorn@tweedegolf.com>

--- openleadr-wire/src/resource.rs
+++ openleadr-wire/src/resource.rs
@@ -4,7 +4,9 @@ use serde_with::{serde_as, skip_serializing_none, DefaultOnNull};
 use std::{fmt::Display, str::FromStr};
 use validator::Validate;
 
-use crate::{target::Target, values_map::ValuesMap, ven::VenId, Identifier, IdentifierError};
+use crate::{
+    target::Target, values_map::ValuesMap, ven::VenId, ClientId, Identifier, IdentifierError,
+};
 
 /// A resource is an energy device or system subject to control by a VEN.
 #[skip_serializing_none]
@@ -19,28 +21,68 @@ pub struct Resource {
     /// datetime in ISO 8601 format
     #[serde(with = "crate::serde_rfc3339")]
     pub modification_date_time: DateTime<Utc>,
-    /// URL safe VTN assigned object ID.
-    #[serde(rename = "venID")]
-    pub ven_id: VenId,
     #[serde(flatten)]
     #[validate(nested)]
-    pub content: ResourceContent,
+    pub content: BlResourceRequest,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase", untagged)]
+pub enum ResourceRequest {
+    BlRequestRequest(BlResourceRequest),
+    VenRequestRequest(VenResourceRequest),
+}
+
+impl Validate for ResourceRequest {
+    fn validate(&self) -> Result<(), validator::ValidationErrors> {
+        match self {
+            ResourceRequest::BlRequestRequest(x) => x.validate(),
+            ResourceRequest::VenRequestRequest(x) => x.validate(),
+        }
+    }
 }
 
 #[skip_serializing_none]
 #[serde_as]
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Validate)]
-#[serde(rename_all = "camelCase", tag = "objectType", rename = "RESOURCE")]
-pub struct ResourceContent {
+#[serde(
+    rename_all = "camelCase",
+    tag = "objectType",
+    rename = "BL_RESOURCE_REQUEST"
+)]
+pub struct BlResourceRequest {
+    #[serde(rename = "clientID")]
+    pub client_id: ClientId,
+    /// A list of targets.
+    pub targets: Option<Vec<Target>>,
     /// User generated identifier, resource may be configured with identifier out-of-band.
     #[serde(deserialize_with = "crate::string_within_range_inclusive::<1, 128, _>")]
     pub resource_name: String,
+    /// VTN provisioned on object creation based on the path, e.g., POST <>/ven/{venID}/resources.
+    #[serde(rename = "venID")]
+    pub ven_id: VenId,
     /// A list of valuesMap objects describing attributes.
     pub attributes: Option<Vec<ValuesMap>>,
-    /// A list of targets.
+}
+
+#[skip_serializing_none]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Validate)]
+#[serde(
+    rename_all = "camelCase",
+    tag = "objectType",
+    rename = "VEN_RESOURCE_REQUEST"
+)]
+pub struct VenResourceRequest {
+    /// User generated identifier, resource may be configured with identifier out-of-band.
+    #[serde(deserialize_with = "crate::string_within_range_inclusive::<1, 128, _>")]
+    pub resource_name: String,
+    /// VTN provisioned on object creation based on the path, e.g., POST <>/ven/{venID}/resources.
+    #[serde(rename = "venID")]
+    pub ven_id: VenId,
+    /// A list of valuesMap objects describing attributes.
     #[serde(default)]
     #[serde_as(deserialize_as = "DefaultOnNull")]
-    pub targets: Vec<Target>,
+    pub attributes: Vec<ValuesMap>,
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Hash, Eq)]
