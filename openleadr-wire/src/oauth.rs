a39857bbd18fece907d92718f329ef28e98344ce
Maximilian Pohl <maximilian@tweedegolf.com>
2026-01-27 14:40:34 +0100

    Add `/auth/server` endpoint
    Signed-off-by: Maximilian Pohl <maximilian@tweedegolf.com>

--- openleadr-wire/src/oauth.rs
+++ openleadr-wire/src/oauth.rs
@@ -1,3 +1,7 @@
+use serde::{Deserialize, Serialize};
+use url::Url;
+use validator::{Validate, ValidateLength, ValidationErrors};
+
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum OAuthErrorType {
@@ -43,5 +47,32 @@ impl OAuthError {
     pub fn with_uri(mut self, uri: String) -> Self {
         self.error_uri = Some(uri);
         self
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AuthServerInfo {
+    #[serde(rename = "tokenURL")]
+    pub token_url: Url,
+}
+
+impl Validate for AuthServerInfo {
+    fn validate(&self) -> Result<(), ValidationErrors> {
+        if self
+            .token_url
+            .as_str()
+            .validate_length(Some(2), Some(8000), None)
+        {
+            return Ok(());
+        }
+
+        let mut err = ::validator::ValidationError::new("length");
+        err.add_param(::std::borrow::Cow::from("min"), &2);
+        err.add_param(::std::borrow::Cow::from("max"), &8000);
+        err.add_param(::std::borrow::Cow::from("value"), &self.token_url);
+
+        let mut errors = ValidationErrors::new();
+        errors.add("token_url", err);
+        Err(errors)
     }
 }
