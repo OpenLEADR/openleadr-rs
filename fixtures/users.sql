1910f54b23636c69a3561580dad25a94d01c38d8
David Venhoek <david@tweedegolf.com>
2026-05-19 16:20:31 +0200

    Allow subscription management also by business logic.
    This is required by the specification.
    
    Signed-off-by: David Venhoek <david@tweedegolf.com>

--- fixtures/users.sql
+++ fixtures/users.sql
@@ -1,13 +1,12 @@
 INSERT INTO "user" (id, reference, description, scopes, created, modified)
-VALUES ('bl-client', 'bl-client-ref', null, '{"read_all", "write_vens_bl", "write_programs", "write_events", "write_users"}', '2024-07-25 08:31:10.776000 +00:00', '2024-07-25 08:31:10.776000 +00:00');
+VALUES ('bl-client', 'bl-client-ref', null, '{"read_all", "write_vens_bl", "write_programs", "write_events", "write_users", "write_subscriptions_bl"}', '2024-07-25 08:31:10.776000 +00:00', '2024-07-25 08:31:10.776000 +00:00');
 
 INSERT INTO user_credentials (user_id, client_id, client_secret)
 VALUES ('bl-client', 'bl-client', '$argon2id$v=19$m=16,t=2,p=1$MWt1QVNFdHdlZVJhNEZzUA$Rmkguwgaz+A2GWIaDRtv8w'); -- secret: bl-client
 
 INSERT INTO "user" (id, reference, description, scopes, created, modified)
-VALUES ('ven-client', 'ven-client-ref', 'desc', '{"read_targets", "read_ven_objects", "write_reports", "write_subscriptions", "write_vens_ven"}', '2024-07-25 08:31:10.776000 +00:00', '2024-07-25 08:31:10.776000 +00:00');
+VALUES ('ven-client', 'ven-client-ref', 'desc', '{"read_targets", "read_ven_objects", "write_reports", "write_subscriptions_ven", "write_vens_ven"}', '2024-07-25 08:31:10.776000 +00:00', '2024-07-25 08:31:10.776000 +00:00');
 
 INSERT INTO user_credentials (user_id, client_id, client_secret)
 VALUES ('ven-client', 'ven-client-client-id',
         '$argon2id$v=19$m=16,t=2,p=1$YWlOSE8xRGFVdVVIa212Ug$tjmQC+zNC3QXc9K8mEXRrA'); -- secret: ven-client
-
